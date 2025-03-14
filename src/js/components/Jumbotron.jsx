449fde9081815a343580c2d229534dcd4694b5fa
Jesus Garland <33094958+jgarlandh@users.noreply.github.com>
2025-03-14 22:31:20 +0000

    feat/Jumbotron.jsx

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Jumbotron = () => {
+  return (
+    <div classNameName="p-5 mb-4 bg-body-tertiary rounded-3">
+      <div className="container-fluid py-5">
+        <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
+        <p className="col-md-12 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quidem perferendis aspernatur, dolorem temporibus consequatur, natus fugit et ullam laudantium corporis ea! Sequi, quasi pariatur!</p>
+        <button className="btn btn-primary btn-lg" type="button">Call to action!</button>
+      </div>
+    </div>
+  )
+}
+
+export default Jumbotron
